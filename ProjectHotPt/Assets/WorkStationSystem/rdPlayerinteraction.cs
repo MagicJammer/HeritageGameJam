8d9bf7f772a9d2ebb59763bbd1fea3c7312279f2
detamine <detamine@gmail.com>
2020-08-01 15:41:32 +0800

    tested interaction system

---
+++ ProjectHotPt/Assets/WorkStationSystem/rdPlayerinteraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rdPlayerinteraction : MonoBehaviour
+{
+    public rdWorkstation SelectedWorkstation;
+    public IngredientType IngredientInHand;
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            SelectedWorkstation.ActivateWorkstation(IngredientInHand);
+        }
+    }
+}
