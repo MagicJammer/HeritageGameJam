8ddb35ba53e864c12f9326175ce7e6210d78c415
detamine <detamine@gmail.com>
2020-08-03 15:34:02 +0800

    created entity with static sprite

---
+++ ProjectRedDotHotPot/Assets/scripts/control/rdEntity.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public enum PlayerState 
+{ 
+    Unassigned,
+    Move, 
+    Work, 
+    //Chat, 
+}
+public class rdEntity : FiniteStateMachine<PlayerState>
+{
+    public override PlayerState UnassignedType => PlayerState.Unassigned;
+    public float MoveSpeed = 10;
+    public float JumpForce = 450;
+    public float GroundCheckDistance = 0.1f;
+    public LayerMask PlatformMask = 1 << 8;
+    [HideInInspector]
+    public Rigidbody2D _RB2D;
+    [HideInInspector]
+    public BoxCollider2D col;
+    [HideInInspector]
+    public float facing = 1;
+    void Start()
+    {
+        _RB2D = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        RegisterState(new MoveState(this));
+        RegisterState(new WorkState(this));
+        ChangeState(PlayerState.Move);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMachine();
+    }
+}
+public class MoveState : rdEntity.SI_State<rdEntity>
+{
+    public override PlayerState StateType => PlayerState.Move;
+    public MoveState(rdEntity brain) : base(brain) { }
+    public override void OnReceiveMessage(int msgtype, object[] args)
+    {
+        PlayerCommand c = (PlayerCommand)msgtype;
+        switch (c)
+        {
+            case PlayerCommand.Walk:
+                float moveX = (float)args[0];
+                if (moveX > 0)
+                    user.facing = 1;
+                else if(moveX<0)
+                    user.facing = -1;
+                if (WallCheck())
+                    return;
+                //Vector2 currentPos = user.transform.position;
+                //currentPos.x += moveX * user.MoveSpeed * Time.deltaTime;
+                //user.transform.position = currentPos;
+                Rigidbody2D r = user._RB2D;
+                r.velocity = new Vector2(moveX * user.MoveSpeed, r.velocity.y);
+                break;
+            case PlayerCommand.Jump:
+                if(GroundCheck())
+                user._RB2D.AddForce(Vector2.up * user.JumpForce);
+                break;
+            case PlayerCommand.Interact:
+                break;
+        }
+    }
+    public bool WallCheck()
+    {
+        float offset = user.col.size.x / 2; ;
+        bool facingWall = Physics2D.Raycast(user.transform.position, Vector2.right * user.facing, offset + 0.1f, user.PlatformMask);
+        return facingWall;
+    }
+    public bool GroundCheck()
+    {
+        BoxCollider2D c = user.col;
+        float footOffset = c.size.x / 2;
+        float heightOffset = c.size.y / 2;
+        Vector2 footPos = user.transform.position;
+        Vector2 footLeft = footPos;
+        footLeft.x -= footOffset;
+        Vector2 footRight = footPos;
+        footRight.x += footOffset;
+        bool leftCheck = Physics2D.Raycast(footLeft, Vector2.down, heightOffset + user.GroundCheckDistance, user.PlatformMask);
+        bool rightCheck= Physics2D.Raycast(footRight, Vector2.down, heightOffset + user.GroundCheckDistance, user.PlatformMask);
+        return leftCheck||rightCheck;
+    }
+    public override void OnStateEnter(PlayerState prevStateType, object[] args)
+    { 
+        user =(rdEntity)Machine;
+    }
+
+    public override void OnStateExit(PlayerState newStateType, object[] arg)
+    { 
+    }
+
+    public override void OnStateUpdate()
+    { 
+    }
+}
+public class WorkState : rdEntity.SI_State<rdEntity>
+{
+    public override PlayerState StateType => PlayerState.Work;
+    public WorkState(rdEntity brain) : base(brain) { }
+    public override void OnReceiveMessage(int msgtype, object[] args)
+    {
+    }
+
+    public override void OnStateEnter(PlayerState prevStateType, object[] args)
+    {
+        user = (rdEntity)Machine;
+    }
+
+    public override void OnStateExit(PlayerState newStateType, object[] arg)
+    {
+    }
+
+    public override void OnStateUpdate()
+    {
+    }
+}
---
+++ ProjectRedDotHotPot/Assets/scripts/core/Deus_FSM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+//namespace Deus
+//{
+    public abstract class FiniteStateMachine<S> : MonoBehaviour where S : struct, Enum
+  {
+        Dictionary<S, Deus_State> _states = new Dictionary<S, Deus_State>();
+        S _currentStateTag;
+        public S CurrentStateType { get { return _currentStateTag; } }
+        public abstract S UnassignedType { get; }
+        Deus_State CurrentState
+        {
+            get
+            {
+                if (_states.TryGetValue(_currentStateTag, out Deus_State currentState))
+                    return currentState;
+                else
+                    return null;
+            }
+        }
+        public void RegisterState(Deus_State state)
+        {
+            _states[state.StateType] = state;
+        }
+        bool _changingState = false;
+        public bool ChangeState(S NextState, params object[] args)
+        {
+            if (NextState.CompareTo(UnassignedType) == 0)
+                return false;
+            if (_changingState)
+                return false;
+            if (_currentStateTag.CompareTo(NextState) == 0)
+                return false;
+            if (!_states.TryGetValue(NextState, out Deus_State newState))
+                return false;
+            _changingState = true;
+            Deus_State currentState = CurrentState;
+            if (currentState != null)
+                currentState.OnStateExit(NextState, args);
+            newState.OnStateEnter(_currentStateTag, args);
+            _currentStateTag = NextState;
+            _changingState = false;
+            return true;
+        }
+        public virtual void UpdateMachine()
+        {
+            var current = CurrentState;
+            if(current!=null)
+                current.OnStateUpdate();
+        }
+
+        public bool HasState(S key)
+        {
+            return _states.ContainsKey(key);
+        }
+        public void SendMessageToBrain(int msgtype, params object[] args)
+        {
+            CurrentState.OnReceiveMessage(msgtype, args);
+        }
+    public abstract class Deus_State
+    {
+        public FiniteStateMachine<S> Machine;
+        public Deus_State(FiniteStateMachine<S> machine)
+        {
+            Machine = machine;
+        }
+        public abstract S StateType { get; }
+        public abstract void OnStateEnter(S prevStateType, object[] args);
+        public abstract void OnStateUpdate();
+        public abstract void OnStateExit(S newStateType, object[] arg);
+        public abstract void OnReceiveMessage(int msgtype, object[] args);
+    }
+    public abstract class SI_State<U> : Deus_State where U : FiniteStateMachine<S>
+    {
+        public U user;
+        public SI_State(U brain) : base(brain) { }
+    }
+  }
+//}
