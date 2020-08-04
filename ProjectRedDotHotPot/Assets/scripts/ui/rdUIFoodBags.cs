7972569b7a0238f4ef6bda950d62d461a04c8603
Dyep-Dev <geoffluismendoza@gmail.com>
2020-08-04 19:27:50 +0800

    added UI scripts

---
+++ ProjectRedDotHotPot/Assets/scripts/ui/rdUIFoodBags.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ingedrient", menuName = "MagicJammer/food bag", order = 2)]
+public class rdUIFoodBags : ScriptableObject
+{
+    public FoodItemTag FoodType;
+    public GameObject Prefab;
+}
---
+++ ProjectRedDotHotPot/Assets/scripts/ui/rdUIManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class rdUIManager : Singleton<rdUIManager>
+{
+    public GameObject PopupPrefab;
+    public float PopupOffsetY = 1.75f;
+
+    public string FoodPath = "ui_foodbags";
+    Dictionary<FoodItemTag, rdUIFoodBags> _foodBags = new Dictionary<FoodItemTag, rdUIFoodBags>();
+
+    public event Action<string> OnDestroyPopup;
+ 
+    protected override void Awake() {
+        base.Awake();
+
+        rdUIFoodBags[] fd= Resources.LoadAll<rdUIFoodBags>(FoodPath);
+        foreach (var f in fd) {
+            _foodBags[f.FoodType] = f;
+        }
+    }
+
+    public static void UpdateStationPopups(GameObject station, List<FoodItemTag> requirements) {
+        requirements.ToArray();
+        UpdateStationPopups(station, requirements);
+    }
+
+    public static void UpdateStationPopups(GameObject station, FoodItemTag[] requirements) {
+        Seele.DestroyPopup(station.name);
+        GameObject popupGO = Instantiate(Seele.PopupPrefab);
+        popupGO.transform.SetParent(station.transform);
+        rdUIWorldSpaceCanvas popupCanvas = popupGO.GetComponent<rdUIWorldSpaceCanvas>();
+        popupCanvas.ID = station.name;
+        RectTransform popupRect = popupGO.GetComponent<RectTransform>();
+        popupRect.localPosition = new Vector3(0, Seele.PopupOffsetY, 0);
+        Transform parent = popupGO.transform.GetChild(0);
+        foreach (FoodItemTag item in requirements) {
+            if (Seele._foodBags.TryGetValue(item, out rdUIFoodBags foodbag)) {
+                GameObject fdGO = Instantiate(foodbag.Prefab);
+                fdGO.transform.SetParent(parent);
+                fdGO.transform.localScale = Vector3.one;
+            }
+        }
+    }
+
+    public void DestroyPopup(string id) {
+        OnDestroyPopup?.Invoke(id);
+    }
+}
