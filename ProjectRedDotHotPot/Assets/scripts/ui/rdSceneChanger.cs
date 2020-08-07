44262d381e4b6676d5562a3d8bb00083dd899fe0
detamine <detamine@gmail.com>
2020-08-07 09:19:38 +0800

    set up basic start scene and music player

---
+++ ProjectRedDotHotPot/Assets/scripts/ui/rdSceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class rdSceneChanger : MonoBehaviour
+{
+    public void GoGameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void GoStartScene()
+    {
+        SceneManager.LoadScene("New Scene");
+
+    }
+}
