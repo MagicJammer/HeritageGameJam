7cd862e74e3cb4f7891e93e85118fa2f586a9868
detamine <detamine@gmail.com>
2020-08-07 09:59:21 +0800

    fix up food bags

--- ProjectRedDotHotPot/Assets/scripts/core/Deus_Seele.cs
+++ ProjectRedDotHotPot/Assets/scripts/core/Deus_Seele.cs
@@ -4,9 +4,17 @@
     public abstract class Singleton<C> : MonoBehaviour where C : MonoBehaviour
     {
         public static C Seele { private set; get; }
+        public bool Perpetual;
         protected virtual void Awake()
         {
+        if (Seele == null)
+        {
             Seele = (C)(MonoBehaviour)this;
+            if (Perpetual)
+                DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
         }
         protected virtual void OnDestroy()
         {
---
+++ ProjectRedDotHotPot/Assets/scripts/ui/rdMusicplayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rdMusicplayer : Singleton<rdMusicplayer>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+    }
+}
